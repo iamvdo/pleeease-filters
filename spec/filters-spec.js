1c129e294d9904d820f9f4bdfa0e3f36bfdecd35
Vincent De Oliveira <vincent@iamvdo.me>
2014-07-02 11:00:26 +0200

    Refactor tests

--- spec/filters-spec.js
+++ spec/filters-spec.js
@@ -3,29 +3,112 @@
 // 1. `npm test`
 
 'use strict';
-var fs       = require('fs');
-var postcss  = require('postcss');
-var __dirname = 'spec/';
-
+var fs     = require('fs');
 var filter = require('../index');
 
-var test     = function (name) {
+var __dirname = 'spec/';
+
+var test = function (name, options) {
 
   // css
   var css = fs.readFileSync(__dirname + name + '.css', 'utf-8');
   var expected = fs.readFileSync(__dirname + name + '.out.css', 'utf-8');
 
   // process
-  var processed = postcss(filter().postcss).process(css).css;
+  var processed = filter.process(css, options);
 
   expect(processed).toBe(expected);
-}
+};
 
 describe('pleeease-filters', function () {
 
-  it('should add SVG filters', function() {
+  it('should not add SVG filters when none', function() {
 
-    test('filters');
+    test('none');
+
+  });
+
+  it('should convert grayscale filters', function() {
+
+    test('grayscale');
+
+  });
+
+  it('should convert sepia filters', function() {
+
+    test('sepia');
+
+  });
+
+  it('should convert saturate filters', function() {
+
+    test('saturate');
+
+  });
+
+  it('should convert hue-rotate filters', function() {
+
+    test('hueRotate');
+
+  });
+
+  it('should convert invert filters', function() {
+
+    test('invert');
+
+  });
+
+  it('should convert opacity filters', function() {
+
+    test('opacity');
+
+  });
+
+  it('should convert brightness filters', function() {
+
+    test('brightness');
+
+  });
+
+  it('should convert contrast filters', function() {
+
+    test('contrast');
+
+  });
+
+  it('should convert blur filters', function() {
+
+    test('blur');
+
+  });
+
+  it('should convert drop-shadow filters', function() {
+
+    test('dropShadow');
+
+  });
+
+  it('should convert multiple filters', function() {
+
+    test('multiple');
+
+  });
+
+  it('should not convert invalid filters', function() {
+
+    test('invalid');
+
+  });
+
+  it('should deal correctly with edge cases', function() {
+
+    test('edge');
+
+  });
+
+  it('should add IE filter when asking', function() {
+
+    test('ie', {oldIE: true});
 
   });
 
